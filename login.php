58d3c069c71ae3c15e9b76ff0cde18ca2d818d5a
mox <a.ramosd@upam.edu.mx>
2025-10-16 18:22:28 -0600

    3.001

--- login.php
+++ login.php
@@ -1,37 +1,29 @@
-<?php
-session_start();
-?>
-
-<!DOCTYPE html>
-<html lang="es">
-
+<?php session_start(); ?>
 <?php include 'include/header.php'; ?>
 
-<!DOCTYPE html>
-<header>
-    <style>
-        h2,
-        form {
-            text-align: center;
-        }
-    </style>
-</header>
-
 <body>
-    <h2> Inicio sesion</h2>
-    <div>
-        <form action="index.php">
-            <label for="user">Nombre de Usuario :</label><br>
-            <input type="text" name="user" id="user"> <br><br>
-            <label for="password">Contraseña :</label> <br>
-            <input type="password" name="password" id="password"> <br>
-            <input type="checkbox" name="TC" id="TC">
-            <label for="TC"> <a href="TC.php">Terminos y condiciones</a></label> <br>
-            <input type="submit" value="entrar">
 
+    <h2>Inicio de Sesión</h2>
+
+    <div class="form-container">
+        <form action="index.php" method="post">
+            <label for="user">Correo electronico:</label>
+            <input type="text" name="user" id="user" required>
+
+            <label for="password">Contraseña:</label>
+            <input type="password" name="password" id="password" required>
+
+            <label>
+                <input type="checkbox" name="TC" id="TC" required>
+                <a href="TC.php" target="blank">Términos y condiciones</a>
+            </label>
+            <a href="/formulario.php">Crear cuenta</a>
+            <input type="submit" value="Entrar">
         </form>
     </div>
 
 </body>
 
 </html>
+
+<?php include 'include/footer.php'; ?>

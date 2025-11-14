3afe05aa46a42e9c4642c6e263d6ccac7297cb66
lab software <a.ramosd@upam.edu.mx>
2025-11-14 08:36:47 -0600

    3.003

---
+++ modelo/prueba.php
@@ -0,0 +1,21 @@
+<?php   
+
+
+$servername = "localhost"; // 127.0.0.1 172.100.28.1  200.100.15.14
+$username = "username";
+$password = "password";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+
+
+
+
+
+?>

a546489412b81c8f14800a228479abbcb4c9e128
mox <a.ramosd@upam.edu.mx>
2025-11-19 19:39:51 -0600

    3.03

--- modelo/prueba.php
+++ modelo/prueba.php
@@ -1,22 +1,15 @@
-<?php   
-
-// hacer base de datos 
-$servername = "localhost"; // 127.0.0.1 172.100.28.1  200.100.15.14
-$username = "mox";
+<?php
+$servername = "localhost";
+$username = "root";
 $password = "";
 
-echo "entraste al modelo" ;
+echo "entraste al modelo";
+
 try {
-  $conn = new PDO("mysql:host=$servername;dbname=prueba", $username, $password);
+  $conn = new PDO("mysql:host=$servername;dbname=miweb", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
-} catch(PDOException $e) {
+} catch (PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
 }
-
-
-
-
-
-?>

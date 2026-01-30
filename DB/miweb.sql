4c1cfa646ed72bbe91897808f0bf83bbefdd0f0b
mox <a.ramosd@upam.edu.mx>
2026-01-29 19:16:35 -0600

    6.0

--- DB/miweb.sql
+++ DB/miweb.sql
@@ -1,56 +1,73 @@
--- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: miweb
--- ------------------------------------------------------
--- Server version	10.4.32-MariaDB
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 30-01-2026 a las 02:14:50
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `usuario`
+-- Base de datos: `miweb`
 --
 
-DROP TABLE IF EXISTS `usuario`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
 CREATE TABLE `usuario` (
-  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `idUsuario` int(11) NOT NULL,
   `nombre` varchar(25) NOT NULL,
   `aPaterno` varchar(25) NOT NULL,
   `aMaterno` varchar(25) NOT NULL,
   `correoElectronico` varchar(25) NOT NULL,
-  `password` varchar(25) NOT NULL,
-  `telefono` varchar(25) NOT NULL,
-  PRIMARY KEY (`idUsuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `password` varchar(255) NOT NULL,
+  `telefono` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `usuario`
+-- Volcado de datos para la tabla `usuario`
 --
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-INSERT INTO `usuario` VALUES (2,'juan','sanchez','martinez','ASD@asd','$2y$10$4bJDm9GJD/zxp3Nz4j','1234567890'),(5,'carlos','franco','flores','123@456','$2y$10$gDGY8tsojRQh07bvyD','1234567890');
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `usuario` (`idUsuario`, `nombre`, `aPaterno`, `aMaterno`, `correoElectronico`, `password`, `telefono`) VALUES
+(7, 'admin', 'admin', 'admin', 'admin@123', '$2y$10$CS1.n12DgWUiR7KcfU', '0000000000'),
+(9, 'qqqq', 'aaa', 'ccc', 'qwer@qwer', '$2y$10$RkZNdLPAgCPakdopasqeOul5p9PcHTKkH72u2Z1hFzq6UpJcU3od.', '1234567890'),
+(10, 'juan', 'perez', 'perez', 'asd@asd', '$2y$10$fHOk.kx.cEEJ7oob1WvmIu6UV1FN4hf7i9aVJzg8.Gg.LwghW3xp6', '1234567890'),
+(11, 'juan', 'perez', 'perez', 'juan@123', '$2y$10$yqa/YfJKpkE5zxeS9ONCj.04u5F3Tt.4LVm47cJr4DzRvDdDJGA0O', '1234567890');
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`idUsuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `idUsuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-12-04 17:51:03
